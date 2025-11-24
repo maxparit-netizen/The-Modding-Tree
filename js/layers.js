ea2330ee6404a82eb7049c827d871300e86f9921
maxparit <max.parit@gmail.com>
2025-11-24 22:18:43 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("p", {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.6, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -24,5 +24,12 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    upgrades: {
+        11: {
+            title: "something or something idk",
+            description: "Double your point gain.",
+            cost: new Decimal(1),
+        }
+    },
     layerShown(){return true}
 })
